945c9c3b75b11e659cdaf537f8c0731bd8a28074
Liqi Xu <liqixu2@illinois.edu>
2016-12-23 17:41:31 +0800

    changes to setup.py

--- setup.py
+++ setup.py
@@ -3,22 +3,23 @@ from setuptools import setup
 setup(
 	name='datahub',
 	version='1.0',
-	# py_modules=['wtfmain', 
-	# 			'db', 
-	# 			'encryption', 
-	# 			'metadata', 
-	# 			'orpheus_const', 
-	# 			'orpheus_exceptions', 
-	# 			'orpheus_sqlparser', 
-	# 			'relation', 
-	# 			'schema_parser', 
-	# 			'user_control', 
-	# 			'version',
-	# 			'access'],
-	py_modules=['click_entry'],
+    py_modules=['main', 
+	 			'db', 
+	 			'encryption', 
+	 			'metadata', 
+	 			'orpheus_const', 
+	 			'orpheus_exceptions', 
+	 			'orpheus_sqlparser', 
+	 			'relation', 
+	 			'schema_parser', 
+	 			'user_control', 
+	 			'version',
+	 			'access',
+	 			'click_entry'],
+	#py_modules=['click_entry'],
 	install_requires=[
-		# 'Click', 'psycopg2', 'PyYAML', 'pandas', 'pyparsing'
-		'Click'
+	    'Click', 'psycopg2', 'PyYAML', 'pandas', 'pyparsing'
+		#'Click'
 	],
 	entry_points='''
 		[console_scripts]
