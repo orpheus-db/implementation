a333bee4cf270415f916237456ffea736e3012ec
silihui2 <silihui2@illinois.edu>
2016-12-28 07:08:53 -0600

    move files into package

--- setup.py
+++ setup.py
@@ -3,26 +3,29 @@ from setuptools import setup
 setup(
 	name='orpheus',
 	version='1.0',
-    py_modules=['main', 
-	 			'db', 
-	 			'encryption', 
-	 			'metadata', 
-	 			'orpheus_const', 
-	 			'orpheus_exceptions', 
-	 			'orpheus_sqlparser', 
-	 			'relation', 
-	 			'schema_parser', 
-	 			'user_control', 
-	 			'version',
-	 			'access',
-	 			'click_entry'],
+	description='OrpheusDB command line tool',
+	packages=['orpheus', 'orpheus.main'],
+	url='http://orpheus-db.github.io/',
+    # py_modules=['db', 
+	 		# 	'encryption', 
+	 		# 	'metadata', 
+	 		# 	'orpheus_const', 
+	 		# 	'orpheus_exceptions', 
+	 		# 	'orpheus_sqlparse', 
+	 		# 	'relation', 
+	 		# 	'orpheus_schema_parser', 
+	 		# 	'user_control', 
+	 		# 	'version',
+	 		# 	'access',
+	 		# 	'click_entry'],
 	#py_modules=['click_entry'],
 	install_requires=[
 	    'Click', 'psycopg2', 'PyYAML', 'pandas', 'pyparsing'
 		#'Click'
 	],
+	license='MIT',
 	entry_points='''
 		[console_scripts]
-		dh=click_entry:cli
+		dh=orpheus.main.click_entry:cli
 	'''
 )
