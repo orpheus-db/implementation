4a22cec374466d36e27e35539034c8004c4403e9
Liqi Xu <liqixu2@illinois.edu>
2016-12-29 17:47:30 +0800

    added tests

--- test/small_table_test.sql
+++ test/small_table_test.sql
@@ -1,17 +1,9 @@
-create table datatable(rid serial primary key, employee_id int, age int, salary float);
-insert into datatable(employee_id, age, salary) values (66000001,25,6500), (66000002,30,7500), (66000001,25,7000), (66000003, 40, 9000), (66000004, 35,6500 ),(66000005,28,7500 ),(66000004, 35,9000 );
+CREATE TABLE index (vid int, rlist int[]);
+INSERT INTO index VALUES (1, ARRAY[1,2]), (2, ARRAY[1,2,3]), (3, ARRAY[1,4,5]), (4, ARRAY[1,4,5,6]);
 
-create table indexTbl (vlist integer[], rlist integer[]);
-insert into indexTbl values ('{1,3,5}', '{1}'), ('{1,2,3,4,5}', '{2}'), ('{2,4}', '{3,4}'), ('{3,4}', '{5}'), ('{3,4,5}','{6}');
+create table datatable(rid serial primary key, employee_id int, age int, salary float);
+insert into datatable(employee_id, age, salary) values (66000001,25,6500), (66000002,30,7500), (66000003,25,7000),(66000004,30,8000), (66000002,30,10000),  (66000005, 35,10000 );
 
 create table version(vid int primary key, num_records int, parent integer[], children integer[], create_time timestamp, commit_time timestamp, commit_msg text);
-insert into version values (1, 3, '{-1}', '{2,3,4}', '2016-03-15 00:00:49.387281-05', '2016-03-15 00:00:49.387281-05', 'test1'),
- (2, 4, '{1}', '{}', '2016-03-15 00:00:49.387281-05', '2016-03-15 00:00:49.387281-05', 'test2'), (3, 4, '{1}', '{4}', '2016-03-15 00:00:49.387281-05',  '2016-03-15 00:00:49.387281-05', 'test3'), (4, 4, '{3}', '{}', '2016-03-15 00:00:49.387281-05',  '2016-03-15 00:00:49.387281-05', 'test4');
-
-create table mapTbl(rid serial primary key, employee_id int);
-create table demo(rid serial primary key, employee_id int);
-
-
-
-
-
+insert into version values (1, 3, '{-1}', '{2,3}', '2016-03-15 00:00:49.387281-05', '2016-03-15 00:00:49.387281-05', 'init version'),
+ (2, 4, '{1}', '{4}', '2016-03-15 00:00:49.387281-05', '2016-03-15 00:00:49.387281-05', 'checkout from v1'), (3, 4, '{1}', '{4}', '2016-03-15 00:00:49.387281-05',  '2016-03-15 00:00:49.387281-05', 'checkout from v1'), (4, 4, '{2,3}', '{}', '2016-03-15 00:00:49.387281-05',  '2016-03-15 00:00:49.387281-05', 'cehckout from v2 and v3');
