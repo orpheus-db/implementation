4b59dc82ef8f1f822f7e60a3bfc624b050d953c5
Liqi Xu <liqixu2@illinois.edu>
2017-04-02 23:51:38 -0500

    partition

--- orpheus/main/partition.py
+++ orpheus/main/partition.py
@@ -1,128 +1,188 @@
 from utility import Node, BiPartiteGraph
 
 class PartitionOptimizer(object):
-		def __init__(self, conn):
+		def __init__(self, conn): #TODO
 			self.conn = conn
-			self.d = __load_index_table(self)
-			self.version_tree = []
-			self.partition = {} # a 2D array, each of which is a vlist of partitions
+			self.versio_tree = dict{}
+			self.root = None
+			self.bipartiteGraph = None
 			self.tolerance = 0
 			self.storage_threshold = 0
 			self.cur_checkout_cost = 0
 
-		# return the delta value of the given interval
-		def __nextDelta(left, right):
-			return 0.5 * (left + right)
+		# initialize the version tree and the corresponding BipartiteGraph
+		def _initialize(self):
+			versionTable = "dataset1_versiontable" #TODO
+			graph_rcnt, graph_ecnt, graph_vlist = 0, 0, set()
+			root, node = None, None
+			version_tree = dict()
 
-		# find the subtree with the root node equals vid
-		def __find_subTree(self, vid):
-			subTree = []
-			for i in versionTree:
-
-		# the fundamental approximate algorithm
-		def __approx(versionT, bipartiteGraph, delta):
-			if bipartiteGraph.calc_threshold() > delta:
-				return sth...
-
-			split_vid = __pick_edge()
-			__find_subTree()
-
-
-		# read index table into a dictionary of format (vid: {rlist})
-		def __load_index_table():
-			table_name = "dataset1_indextable"
-			sql = "SELECT * FROM %s ;" % table_name
-			self.conn.cursor.execute(sql)
-			return dict(self.conn.cursor.fetchall())
-
-		# Read the version graph and convert it to a tree
-		# Convert from the version graph to the version tree
-		def __construct_version_tree():
-			table_name = "dataset1_versiontable"
-			sql = "SELECT vid, parent, children, commonRCnt" % table_name
+			sql = "SELECT vid, rCnt, parents, children, commonRCnt FROM  %s" % versionTable
 			self.conn.cursor.execute(sql)
 			for x in self.conn.cursor.fetchall():
-				vid, parent, children, commonRCnt = x[0], x[1], x[2], x[3]
-				if (len(parent) > 1):
+				vid, rCnt, parents, children, commonRCnt = x[0], x[1], x[2], x[3], x[4]
+
+				max_index = 0
+				if (len(parents) > 1):
 					max_idx = commonRCnt.index(max(commonRCnt))
-				else:
-					max_idx = 0
+
+				graph_vlist.append(vid)
+				graph_rcnt += rCnt - commonRCnt[max_idx]
+				graph_ecnt += rCnt
+
 				node = Node(vid, children, parent[max_idx], commonRCnt[max_idx])
-				self.versionTree.append(node)
+				self.version_tree[vid] = node
 
-		def __is_valid_edge(self, commonRCnt):
-			return commonRCnt <= self.delta * self.bipartiteGraph.getRCnt()
+				if len(parents) == 1 and parents[0] == -1:
+					self.root = node
 
-		# pick the edge to cut
-		def __pick_edge(self):
-			vid, maxCommonRCnt = 0
-			for node in self.versionTree:
-				commonRCnt = getCommonRCnt()
-				if (__is_valid_edge(commonRCnt) and commonRCnt > maxCommonRCnt):
-					vid = node.getVid()
-					node.getCommonRCnt() = maxCommonRCnt
-			return vid
+			self.bipartiteGraph = BipartiteGraph(graph_vlist, graph_rcnt, graph_ecnt)
 
-		# check if current average checkout cost > \mu * optimal one (opt_partition())
-		def exceeds_tolarated_checkout_cost():
-			# return curCheckoutCost > \mu * opt_partition()
-			return False
+		def _is_valid_edge(self, commonRCnt, rCnt, delta):
+			return commonRCnt <= self.delta * rCnt
 
-		# invoked during the commit
-		# if commonCnt < prev_delta * |R| and S < gamma,
-		# 	create a new version (partition?)
-		# else
-		# 	add it to its parent partition table
-		# update_partition_table()
-		# compare_with_opt()
-		# migration()
-		def commit_assign():
-			return None
-		# update the partition table with the corresponding partition mapping
-		def update_partition_table():
-			return None
-		#migration from one partition scheme to the opt one
-		def migration():
-			return None
+		# traverse through the tree starting at the Root
+		# and pick the edge with the smallest number of commonRCnt(aka weight)
+		def _pick_edge(self, root, rCnt, delta):
+			minNode, minCommonRCnt = None, 0
+			visited, stack = set(), [root]
+			while stack:
+				node = stack.pop()
+				if node not in visited:
+					visited.add(node)	
+					for vid in node.getChildren():
+						stack.append(self.version_tree[vid])
+					if node.getParent() == None:
+						continue
+					commonRnt = node.getCommonRnt()
+					if (_is_valid_edge(commonRCnt, rCnt, delta) and commonRCnt < minCommonRCnt):
+						minNode = node
+						minCommonRCnt = commonRnt
+			return minNode
+
+		# return the delta value of the given interval
+		def _nextDelta(self, left, right):
+			return 0.5 * (left + right)
+
+		# construct the subTree and bipartiteGraph at the split_node
+		def _splitGraph(self, split_node):
+			visited, stack = set(), [split_node]
+			vlist, rCnt, eCnt, commonRCnt = set(), 0, 0, 0
+
+			while stack:
+				node = stack.pop()
+				if node not in visited:
+					visited.add(node)
+					for vid in node.getChildren():
+						stack.append(version_tree[vid])
+					# ======== Update bipartiteGraph statistics
+					cur_vid = node.getVid()
+					vlist.append(cur_vid)
+					eCnt += node.getRCnt()
+					rCnt += node.getRCnt() - node.getCommonRnt()
+					commonRCnt += node.getCommonRnt()
+			
+			version_tree[split_node.getParent()].setChildren(None)
+
+			newBipartiteGraph = BipartiteGraph(vlist, rCnt, eCnt)
+			partitions.add(newBipartiteGraph)
+			# Find the old partition it is in, and 
+			# update the old partition statistics
+			parentBipartiteGraph = None
+			for bipartiteGraph in partitions:
+				if bipartiteGraph.getVList() & vlist:
+					bipartiteGraph.update(bipartiteGraph.getVList() - vlist,
+										  biPartiteGraph.getVCnt() - vCnt, 
+										  biPartiteGraph.getRCnt() - rCnt + split_node.getCommonRnt(),
+										  biPartiteGraph.getECnt() - eCnt)
+					parentBipartiteGraph = biPartiteGraph
+
+			return newBipartiteGraph, parentBipartiteGraph
+			# TODO: Err if there is more than one such bipartite Graph 
+
+		# read index table into a dictionary of format (vid: {rlist})
+		# def _load_index_table():
+		# 	table_name = "dataset1_indextable"
+		# 	sql = "SELECT * FROM %s ;" % table_name
+		# 	self.conn.cursor.execute(sql)
+		# 	return dict(self.conn.cursor.fetchall())
+
+		# the fundamental approximate algorithm
+		def _approx(self, root, bipartiteGraph, delta, partitions):
+			if bipartiteGraph.calc_threshold() > delta:
+				return
+			split_node = _pick_edge(root, bipartiteGraph.getRCnt(), delta)
+			newBipartiteGraph, parentBipartiteGraph = _splitGraph(self, split_node)
+			_approx(split_node, newBipartiteGraph, delta)
+			_approx(root, parentBipartiteGraph, delta)
+			return 
+
+
+		# def _calc_storage_threshold(partitions):
+		# 	storage = 0
+		# 	for bipartiteGraph in partitions:
+		# 		storage += partitions.getRCnt()
+		# 	return storage
+
+		# # check if current average checkout cost > \mu * optimal one (opt_partition())
+		# def exceeds_tolarated_checkout_cost():
+		# 	# return curCheckoutCost > \mu * opt_partition()
+		# 	return False
+
+		# # invoked during the commit
+		# # if commonCnt < prev_delta * |R| and S < gamma,
+		# # 	create a new version (partition?)
+		# # else
+		# # 	add it to its parent partition table
+		# # update_partition_table()
+		# # compare_with_opt()
+		# # migration()
+		# def commit_assign():
+		# 	return None
+		# # update the partition table with the corresponding partition mapping
+		# def update_partition_table():
+		# 	return None
+		# #migration from one partition scheme to the opt one
+		# def migration():
+		# 	return None
 
 		def opt_partition():
-			#initial interval of delta
-			eCnt = sum([len(i) for i in self.d.values()])
-			vCnt = len(self.d)
-			s = set()
-			for i in self.d.values():
-				s |= set(i)
-			rCnt = len(s)
+			_initialize()
 
-			bipartiteGraph = BipartiteGraph(rCnt, vCnt, eCnt)
-
-			left = float(eCnt)/(rCnt * vCnt); right = 1
-			delta = __nextDelta(left, right)
-
+			left = self.bipartiteGraph.calc_threshold()
+			right = 1
+			
+			#binary search for best \delta
 			while left < right:
-				__approx(versionT, bipartiteGraph, delta)
+				partitions = {} # each element is a bipartiteGraph
+				delta = __nextDelta(left, right)
+				__approx(self.root, self.bipartiteGraph, delta, partitions)
+				cur_storage = __calc_storage_threshold(partitions)
+				if cur_storage < self.storage_threshold:
+					left = delta
+				else:
+					right = delta
 
-			return None
-			# return optimal checkout cost
+		# 	# return optimal checkout cost
 
-		# return the corresponding partition table of the given vid
-		# TODO: fix the table_name
-		def partition_lookup(self, table_name, vid):
-			## schema of the partitioning table
-			## pid, {vid, ...},rCnt,vCnt (optional)
-			sql = "SELECT pid FROM %s WHERE %s = ANY(vlist)" % (table_name, vid)
-			self.conn.cursor.execute(sql)
-			res = [x[0] for x in self.conn.cursor.fetchall()]
-			if len(res) != 1:
-				print "=========== ERROR in partition_lookup() ==============="
-				#TODO: change to Exception
-			#print res
-			return res
+		# # return the corresponding partition table of the given vid
+		# # TODO: fix the table_name
+		# def partition_lookup(self, table_name, vid):
+		# 	## schema of the partitioning table
+		# 	## pid, {vid, ...},rCnt,vCnt (optional)
+		# 	sql = "SELECT pid FROM %s WHERE %s = ANY(vlist)" % (table_name, vid)
+		# 	self.conn.cursor.execute(sql)
+		# 	res = [x[0] for x in self.conn.cursor.fetchall()]
+		# 	if len(res) != 1:
+		# 		print "=========== ERROR in partition_lookup() ==============="
+		# 		#TODO: change to Exception
+		# 	#print res
+		# 	return res
 
-		def unitTest(self):
-			table_name = "dataset1_indextable";
-			sql = "SELECT * FROM %s ;" % table_name
-			self.conn.cursor.execute(sql)
-			d = dict(self.conn.cursor.fetchall())
-			print d
-			#return res
+		# def unitTest(self):
+		# 	table_name = "dataset1_indextable";
+		# 	sql = "SELECT * FROM %s ;" % table_name
+		# 	self.conn.cursor.execute(sql)
+		# 	d = dict(self.conn.cursor.fetchall())
+		# 	print d
+		# 	#return res
--- orpheus/main/utility.py
+++ orpheus/main/utility.py
@@ -1,31 +1,62 @@
 class Node(object):
-    def __init__(self, vid, children, parent, commonRCnt):
+
+    def __init__(self, vid, rCnt, children, parent, commonRCnt):
       self.vid = vid
+      self.rCnt = rCnt
       self.children = children
-      self.parent = parent
+      if len(parents) == 1 and parents[0] == -1:
+        self.parent = None
+      else:
+        self.parent = parent
       self.commonRCnt = commonRCnt
-    def getChildren(self):
-      return self.children
-    def getParent(self):
-      return self.parent
-    def getCommonRCnt(self):
-      return self.commonRCnt
+
     def getVid(self):
       return self.vid
 
-class BipartiteGraph(object):
-  def __init__(self, rCnt, vCnt, eCnt):
-      self.rCnt = rCnt
-      self.vCnt = vCnt
-      self.eCnt = eCnt3
-  def getRCnt(self):
+    def getRCnt(self):
       return self.rCnt
-  def getvCnt(self):
-      return self.vCnt
-  def geteCnt(self):
-      return self.eCnt
-  def calc_threshold(self):
-      return float(self.eCnt) / (self.rCnt + self.vCnt)
 
+    def getChildren(self):
+      return self.children
+
+    def getParent(self):
+      return self.parent
+
+    def getCommonRCnt(self):
+      return self.commonRCnt
+
+    def setParent(self, parent):
+      self.parent = parent 
+      
+    def setChildren(self, children):
+      self.children = children
+
+# Each BipartiteGraph corresponding to a partition
+class BipartiteGraph(object):
+
+  def __init__(self, vlist=set(), rCnt, eCnt):
+      self.vlist = vlist
+      self.rCnt = rCnt
+      self.eCnt = eCnt
+
+  def getVList(self):
+    return self.vlist
+
+  def getRCnt(self):
+    return self.rCnt
+
+  def getECnt(self):
+    return self.eCnt
+
+  def getVCnt(self):
+    return len(self.vlist)
+
+  def update(self, vlist, rCnt, eCnt):
+    self.vlist = vlist
+    self.rCnt = rCnt
+    self.eCnt = eCnt
+
+  def calc_threshold(self):
+    return float(self.eCnt) / (self.rCnt + len(self.vlist))
 
 
